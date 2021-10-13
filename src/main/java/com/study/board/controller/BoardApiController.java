ec30c91237319982d1b9b0f32af0d9f1381e8477
DESKTOP-GA8CAA3\JUNG <doizz@naver.com>
2021-10-13 22:30:16 +0900

    CRUD 기능 추가

--- src/main/java/com/study/board/controller/BoardApiController.java
+++ src/main/java/com/study/board/controller/BoardApiController.java
@@ -1,18 +1,44 @@
 package com.study.board.controller;
 
+import com.study.board.dto.BoardRequestDto;
+import com.study.board.dto.BoardResponseDto;
+import com.study.board.model.BoardService;
 import com.study.exception.CustomException;
 import com.study.exception.ErrorCode;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class BoardApiController {
 
-    @GetMapping("/test")
-    public String test() {
-        throw new CustomException(ErrorCode.POSTS_NOT_FOUND);
+    private final BoardService boardService;
+
+    /**
+     * 게시글 생성
+     */
+    @PostMapping("/boards")
+    public Long save(@RequestBody final BoardRequestDto params) {
+        return boardService.save(params);
+    }
+
+    /**
+     * 게시글 리스트 조회
+     */
+    @GetMapping("/boards")
+    public List<BoardResponseDto> findAll() {
+        return boardService.findAll();
+    }
+
+    /**
+     * 게시글 수정
+     */
+    @PatchMapping("/boards/{id}")
+    public Long save(@PathVariable final Long id, @RequestBody final BoardRequestDto params) {
+        return boardService.update(id, params);
     }
 
 }
